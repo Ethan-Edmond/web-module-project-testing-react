8b23612a1b9a4548374fcd3b846fdf7f49680799
Ethan-Edmond <eedmond@uccs.edu>
2021-06-14 15:18:35 -0600

    Added render test for Display

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -1,21 +1,14 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 ///Tasks:
 //1. Add in nessisary imports and values to establish the testing suite.
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Display from '../Display';
+
 //2. Test that the Display component renders without any passed in props.
+test('Display component renders without props', () => {
+  render(<Display/>);
+});
 //3. Rebuild or copy a show test data element as used in the previous set of tests.
 //4. Test that when the fetch button is pressed, the show component will display. Make sure to account for the api call and change of state in building your test.
 //5. Test that when the fetch button is pressed, the amount of select options rendered is equal to the amount of seasons in your test data.
